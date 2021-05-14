43c99a3e6dda280792b3bfe7e48d539f505da673
Matheus Lima <matheusmt31@gmail.com>
2021-05-13 22:52:47 -0300

    add dependencia morgan para monitorar requisicoes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos  = require('./routes/pedidos');
 
+app.use(morgan('dev'));
 app.use('/pedidos', rotaPedidos);
 app.use('/produtos', rotaProdutos);
 app.use((req, res, next)=>{

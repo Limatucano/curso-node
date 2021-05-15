a7b5aa723fd8adf36f7c74474b388ac79c8b59f8
Matheus Lima <matheusmt31@gmail.com>
2021-05-14 22:51:12 -0300

    incluida conexao com mariadb, adicao de crud e env

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
-
+const mysql = require('../mysql').pool;
 
 router.get('/', (req, res, next)=>{
-    res.status(200).send({
-        mensagem:'get ai',
-    })
+
+    mysql.getConnection((error,connection)=>{
+        if(error){return res.status(500).send({error:error})}
+        connection.query(
+            'SELECT * FROM produtos',
+            (error, resultado, field)=>{
+                connection.release();
+
+                if(error){
+                    res.status(500).send({
+                        error: error,
+                    })
+                }
+                res.status(200).send({
+                    message:resultado,
+                });
+            }
+        )
+    });
 });
 
 router.post('/', (req, res, next)=>{
@@ -13,34 +29,85 @@ router.post('/', (req, res, next)=>{
     const produto = {
         nome: req.body.nome,
         preco: req.body.preco,
-    }
-    
-    res.status(201).send({
-        mensagem: `${produto.nome} - ${produto.preco}`,
+    };
+
+    mysql.getConnection((error, connection)=>{
+        if(error){return res.status(500).send({error:error})}
+        connection.query(
+            'INSERT INTO produtos (nome, preco) VALUES (?, ?)',
+            [produto.nome, produto.preco],
+            (error, resultado, field) =>{
+                connection.release();
+
+                if(error){
+                    res.status(500).send({
+                        error: error,
+                        response:null
+                    });
+                }
+
+                res.status(201).send({
+                    mensagem: "Produto inserido com sucesso",
+                    produto: resultado.insertId,
+                })
+            }
+        )
     })
+    
 });
 
 router.get('/:id_produto', (req, res, next)=>{
     const id_produto = req.params.id_produto;
 
+    mysql.getConnection((error, connection)=>{
+        if(error){return res.status(500).send({error:error})}
+        connection.query(
+            'SELECT * FROM produtos WHERE id_produto = ?',
+            [id_produto],
+            (error, resultado, field)=>{
+                connection.release();
 
-    res.status(200).send({
-        mensagem: `id do produto ${id_produto}`
-    })
+                if(error){
+                    res.status(500).send({
+                        error: error,
+                    });
+                }
+                res.status(200).send({
+                    message: resultado
+                })
+            }
+        )
+    });
 });
 
 router.patch('/', (req, res, next)=>{
-
+    //UPDATE users SET name = $1, email = $2 WHERE id = $3'
     res.status(200).send({
         mensagem: `patch ai`
     })
 });
 
 
-router.delete('/', (req, res, next)=>{
+router.delete('/:id_produto', (req, res, next)=>{
+    const id_produto = req.params.id_produto;
+    mysql.getConnection((error, connection)=>{
 
-    res.status(200).send({
-        mensagem: 'delete ai'
+        connection.query(
+            'DELETE FROM produtos WHERE id_produto = ?',
+            [id_produto],
+            (error, resultado, field)=>{
+                connection.release();
+                if(error){
+                    res.status(500).send({
+                        error: error
+                    })
+                }
+                res.status(204).send({
+                    message: "deletado com sucesso",
+                })
+            }
+        )
+
     })
 });
 
